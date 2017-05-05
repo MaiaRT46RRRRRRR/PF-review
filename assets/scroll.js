eda732b2da3ae30b217bc3b8671a437393ad79d1
Maia <maia.rt.46@gmail.com>
2017-05-05 15:07:33 -0500

    todo lo anterior

---
+++ assets/scroll.js
@@ -0,0 +1,14 @@
+	var lastScrollTop = 0;
+	window.addEventListener("scroll", function(){
+   		var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+   			if (currentScroll > lastScrollTop){
+   				 document.getElementById("nav-header").classList.add("scrollon");
+                document.getElementsByClassName("lista").style.color = "#333447";
+   			} else {
+   				  document.getElementById("nav-header").classList.add("scrollon");
+   				    if(currentScroll<=3){
+   				      document.getElementById("nav-header").classList.remove("scrollon");
+   				    }
+   			}
+   				lastScrollTop = currentScroll;
+	}, false);
